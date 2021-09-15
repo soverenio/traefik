ec1bba2cf86b5bfa2b275f6b073dd9f0ec8e3135
Maria Zubareva <maria.zubareva@insolar.io>
2021-09-15 19:26:58 +0300

    SMAT-121: add request_id to logs

--- pkg/middlewares/replicate/replicate_test.go
+++ pkg/middlewares/replicate/replicate_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/soverenio/traefik/pkg/middlewares/producer"
+	"github.com/traefik/traefik/v2/pkg/middlewares/producer"
 )
 
 func TestReplicate(t *testing.T) {
