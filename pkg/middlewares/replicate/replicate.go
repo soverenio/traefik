7734b42db0a84a5ebf299328f2072ca106f58be5
Maria Zubareva <maria.zubareva@insolar.io>
2021-09-16 12:57:15 +0300

    SMAT-121: move producer

--- pkg/middlewares/replicate/replicate.go
+++ pkg/middlewares/replicate/replicate.go
@@ -14,7 +14,7 @@ import (
 	"github.com/traefik/traefik/v2/pkg/config/runtime"
 	"github.com/traefik/traefik/v2/pkg/log"
 	"github.com/traefik/traefik/v2/pkg/middlewares"
-	"github.com/traefik/traefik/v2/pkg/middlewares/producer"
+	"github.com/traefik/traefik/v2/pkg/middlewares/replicate/producer"
 	"github.com/traefik/traefik/v2/pkg/safe"
 )
 
--- pkg/middlewares/replicate/replicate_test.go
+++ pkg/middlewares/replicate/replicate_test.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/traefik/traefik/v2/pkg/middlewares/producer"
+	"github.com/traefik/traefik/v2/pkg/middlewares/replicate/producer"
 )
 
 func TestReplicate(t *testing.T) {
