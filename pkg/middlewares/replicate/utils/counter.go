05aaf9e03e2846c01ff8378769eb3ae2aacb1cec
Maria Zubareva <maria.zubareva@insolar.io>
2021-10-15 15:14:00 +0300

    SMAT-191: monitor requests

--- pkg/middlewares/replicate/utils/counter.go
+++ pkg/middlewares/replicate/utils/counter.go
@@ -4,15 +4,18 @@ import (
 	"sync/atomic"
 )
 
+// SyncCounter safely counts events in parallel threads.
 type SyncCounter interface {
 	Inc()
 	Add(uint64)
 	Load() uint64
 }
+
 type syncCounter struct {
 	count uint64
 }
 
+// Load returns current value and reset it.
 func (c *syncCounter) Load() uint64 {
 	for {
 		val := atomic.LoadUint64(&c.count)
@@ -22,14 +25,17 @@ func (c *syncCounter) Load() uint64 {
 	}
 }
 
+// Inc increments counter.
 func (c *syncCounter) Inc() {
 	atomic.AddUint64(&c.count, 1)
 }
 
+// Add adds delta to counter.
 func (c *syncCounter) Add(delta uint64) {
 	atomic.AddUint64(&c.count, delta)
 }
 
+// NewSyncCounter create new SyncCounter.
 func NewSyncCounter() SyncCounter {
 	return &syncCounter{}
 }
