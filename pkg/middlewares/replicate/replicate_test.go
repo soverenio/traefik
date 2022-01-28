7d490ce1fa2b75b2c02a5722afb128152206b447
Maria Zubareva <maria.zubareva@insolar.io>
2022-01-28 17:55:28 +0300

    SMAT-423: body as bytes

--- pkg/middlewares/replicate/replicate_test.go
+++ pkg/middlewares/replicate/replicate_test.go
@@ -14,7 +14,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"github.com/traefik/traefik/v2/pkg/middlewares/replicate/producer"
 	"github.com/traefik/traefik/v2/pkg/middlewares/replicate/utils"
 )
