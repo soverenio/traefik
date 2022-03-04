ba60f44261940a34625238996e79ab69f515616a
Sergey Morozov <ita.sammann@gmail.com>
2022-03-04 16:56:38 +0300

    SMAT-476: Keep vendor dir in repo (#32)

---
+++ vendor/github.com/hashicorp/consul/agent/connect/uri_service.go
@@ -0,0 +1,28 @@
+package connect
+
+import (
+	"fmt"
+	"net/url"
+)
+
+// SpiffeIDService is the structure to represent the SPIFFE ID for a service.
+type SpiffeIDService struct {
+	Host       string
+	Namespace  string
+	Datacenter string
+	Service    string
+}
+
+// URI returns the *url.URL for this SPIFFE ID.
+func (id *SpiffeIDService) URI() *url.URL {
+	var result url.URL
+	result.Scheme = "spiffe"
+	result.Host = id.Host
+	result.Path = fmt.Sprintf("/ns/%s/dc/%s/svc/%s",
+		id.Namespace, id.Datacenter, id.Service)
+	return &result
+}
+
+func (id *SpiffeIDService) CommonName() string {
+	return ServiceCN(id.Service, id.Namespace, id.Host)
+}
---
+++ vendor/github.com/mailgun/minheap/go.mod
@@ -0,0 +1,5 @@
+module github.com/mailgun/minheap
+
+go 1.12
+
+require github.com/stretchr/testify v1.3.0
